652ac416492ee3077719b7a889c7582d679b9bdd
Mathew Sida-Nguon (H10 Capital) <sida.mathew@gmail.com>
2023-02-16 22:21:09 -0800

    p1

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -6,7 +6,17 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    id: 
+    {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: 
+    {
+      type: DataTypes.STRING
+    }
   },
   {
     sequelize,
@@ -16,5 +26,4 @@ Tag.init(
     modelName: 'tag',
   }
 );
-
 module.exports = Tag;
